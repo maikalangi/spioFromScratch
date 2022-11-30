3e5c8a3cc8b4a4e02c84b3367366416d8739df87
maikalangi <maikalangi@gmail.com>
2022-11-29 17:06:50 -0800

    header progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import spio from './spio.png';
+import search from './search.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={spio} alt="SPIO logo" />
+        South Pacific Islander Organization
+        <input type="search" className='search'/>
+        <button className='material-symbols-outlined'><img src={search} alt="search" /></button>
+        <button className='donate'>DONATE</button>
       </header>
     </div>
   );
