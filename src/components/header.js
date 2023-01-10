a1343c6899cdcb4c16ace1f088e1e7648ab5ad12
maikalangi <maikalangi@gmail.com>
2023-01-10 01:59:36 -0800

    header placement  adjustment

--- src/components/header.js
+++ src/components/header.js
@@ -5,18 +5,20 @@ import Nav from './nav';
 
 function Header() {
     return (
-      <header className="App-header">
-        <div className='logo-header'>
-          <img className='logo' src={spio} alt="SPIO" />
-          <span className='title'>South Pacific Islander Organization</span>
-        </div>
-        <input type="search" className='search-bar'/>
-        <button className='search-icon'><img src={search} alt="search" /></button>
-        <button className='donate-button'>DONATE</button>
-        <div className='nav-bar'>
-          <Nav/>
-        </div>
-      </header>
+      <>
+        <header className="header">
+          <div className='page-top'>
+            <div className='logo-header'>
+              <img className='logo' src={spio} alt="SPIO" />
+              <span className='title'>South Pacific Islander Organization</span>
+            </div>
+            <input type="search" className='search-bar' />
+            <button className='search-icon'><img src={search} alt="search" /></button>
+            <button className='donate-button'>DONATE</button>
+          </div>
+          <Nav />
+        </header>
+      </>
     );
 }
 
