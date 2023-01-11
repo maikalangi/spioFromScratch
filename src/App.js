f97aa30afc060e7fd360ad03064f8f4e227fe08c
maikalangi <maikalangi@gmail.com>
2023-01-10 17:23:55 -0800

    started carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/header';
+import Carousel from './components/carousel';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Carousel />
     </div>
   );
 }
---
+++ src/components/carousel.js
@@ -0,0 +1,11 @@
+import './carousel.css';
+
+function Carousel() {
+    return (
+        <ul className='carousel'>
+
+        </ul>
+    );
+}
+
+export default Carousel;
