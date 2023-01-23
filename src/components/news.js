20b68e133fdcc4c50095c25b98a6f675ee691196
maikalangi <maikalangi@gmail.com>
2023-01-22 21:10:36 -0800

    new additions

--- src/components/news.js
+++ src/components/news.js
@@ -2,9 +2,26 @@ import '../styles/news.css';
 
 function News() {
   return (
-    <div>
-
-    <h3>NEWS</h3>
+    <div className='news'>
+      <h3>NEWS</h3>
+      <div className='newsDiv'>
+        <div className='newsBlock'>
+          <h5>Title</h5>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, porro. Architecto libero rerum ab fugiat officia quae quos, perspiciatis ut quas natus omnis deleniti voluptate dolorum earum blanditiis tenetur reprehenderit!</p>
+        </div>
+        <div className='newsBlock'>
+        <h5>Title</h5>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, porro. Architecto libero rerum ab fugiat officia quae quos, perspiciatis ut quas natus omnis deleniti voluptate dolorum earum blanditiis tenetur reprehenderit!</p>
+        </div>
+        <div className='newsBlock'>
+        <h5>Title</h5>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, porro. Architecto libero rerum ab fugiat officia quae quos, perspiciatis ut quas natus omnis deleniti voluptate dolorum earum blanditiis tenetur reprehenderit!</p>
+        </div>
+        <div className='newsBlock'>
+        <h5>Title</h5>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, porro. Architecto libero rerum ab fugiat officia quae quos, perspiciatis ut quas natus omnis deleniti voluptate dolorum earum blanditiis tenetur reprehenderit!</p>
+        </div>
+      </div>
     </div>
   )
 }
