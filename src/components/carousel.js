f185dff5e419219a8462ccdf5c2f94a6ef5c7063
maikalangi <maikalangi@gmail.com>
2023-01-27 17:15:16 -0800

    carousel adjustments

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -9,7 +9,7 @@ function Carousel() {
         <ul className='carousel'>
             <li className='slides' id='slide-1'>
                 <div>
-                    <p>We are breaking Pacific Islander stereotypes and sharing our stories.</p>
+                    <h5>We are breaking Pacific Islander stereotypes and sharing our stories.</h5>
                     <p>Learn about Fijian entrepreneur with M.A. in Gerontology, Ana Jones, and the launching of her app that connects elders with caregivers.</p>
                 </div>
                 <img src={slide1} alt="" />
@@ -17,19 +17,19 @@ function Carousel() {
             <li className='slides' id='slide-2'>
                 <img src={slide2} alt="" />
                 <div>    
-                    <p>We are building collective Pacific power, beyond country lines.</p>
+                    <h5>We are building collective Pacific power, beyond country lines.</h5>
                     <p>Read about Native Hawaiian professional MMA fighter, Ilima-Lei Macfarlane, and her thoughts on the importance of community away from home, higher education, and the collective support for larger Indigenous communities.</p>
                 </div>
             </li>
             <li className='slides' id='slide-3'>
-                <p>We are democratizing Pacific Islander access to educational and career resources.</p>
+                <h5>We are democratizing Pacific Islander access to educational and career resources.</h5>
                 <img src={slide3} alt="" />
                 <p>Congratulate one of SPIO’s 2021 Honorary Scholarship receipients, Vinessa Dueñas. Click the link to learn more about Vinessa’s efforts in serving her community.</p>
             </li>
             <li className='slides' id='slide-4'>
                 <img src={slide4} alt="" />
                 <div>                    
-                    <p>We are amplifying Pacific Islander presence in higher education and professional fields, globally.</p>
+                    <h5>We are amplifying Pacific Islander presence in higher education and professional fields, globally.</h5>
                     <p>Dive into Finausina Tovo’s discussion about her academic journey, the importance of giving Pacific Islanders a voice in higher education, and the Mana program’s effect on Pacific scholars. </p>
                 </div>
             </li>
